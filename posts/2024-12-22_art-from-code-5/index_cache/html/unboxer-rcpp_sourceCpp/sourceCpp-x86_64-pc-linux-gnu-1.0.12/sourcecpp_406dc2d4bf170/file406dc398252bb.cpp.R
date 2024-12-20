276f862a513094506b083f4bd00d07eed785b372
Danielle Navarro <djnavarro@protonmail.com>
2024-12-20 12:55:23 +1100

    cleans up posts 1-5

---
+++ posts/2024-12-22_art-from-code-5/index_cache/html/unboxer-rcpp_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.12/sourcecpp_406dc2d4bf170/file406dc398252bb.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/home/danielle/GitHub/djnavarro/blog/posts/2024-12-22_art-from-code-5/index_cache/html/unboxer-rcpp_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.12/sourcecpp_406dc2d4bf170/sourceCpp_2.so')
+
+unboxer_rcpp <- Rcpp:::sourceCppFunction(function(iterations, layers) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_unboxer_rcpp')
+
+rm(`.sourceCpp_1_DLLInfo`)
