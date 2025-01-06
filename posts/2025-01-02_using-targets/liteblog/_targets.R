6a5a4bfe65bd4bc4c40fae625c2b349e4d447f53
Danielle Navarro <djnavarro@protonmail.com>
2025-01-06 19:53:43 +1100

    expands description of liteblog

--- posts/2025-01-02_using-targets/liteblog/_targets.R
+++ posts/2025-01-02_using-targets/liteblog/_targets.R
@@ -7,26 +7,50 @@ list(
   tar_target(
     name = blog,
     command = Liteblog$new(
-      root   = rprojroot::find_root(rprojroot::has_file("_liteblog.R")),
+      root = rprojroot::find_root(
+        rprojroot::has_file("_liteblog.R")
+      ),
       source = "source",
       output = "site",
-      url    = "liteblog.djnavarro.net"
+      url = "liteblog.djnavarro.net"
     )
   ),
 
   # track configuration files
-  tar_target(blog_rds, saveRDS(blog, file = "_liteblog.rds"), format = "file"),
+  tar_target(
+    name = blog_rds,
+    command = saveRDS(blog, file = "_liteblog.rds"),
+    format = "file"
+  ),
   tar_target(blog_css, "_liteblog.css", format = "file"),
   tar_target(blog_hdr, "_liteblog-header.html", format = "file"),
   tar_target(blog_ftr, "_liteblog-footer.html", format = "file"),
 
   # detect file paths (always run)
-  tar_target(post_paths, blog$find_posts(), cue = tar_cue("always")),
-  tar_target(static_paths, blog$find_static(), cue = tar_cue("always")),
+  tar_target(
+    name = post_paths,
+    command = blog$find_posts(),
+    cue = tar_cue("always")
+  ),
+  tar_target(
+    name = static_paths,
+    command = blog$find_static(),
+    cue = tar_cue("always")
+  ),
 
   # specify file targets
-  tar_target(post_files, post_paths, pattern = map(post_paths), format = "file"),
-  tar_target(static_files, static_paths, pattern = map(static_paths), format = "file"),
+  tar_target(
+    name = post_files,
+    command = post_paths,
+    pattern = map(post_paths),
+    format = "file"
+  ),
+  tar_target(
+    name = static_files,
+    command = static_paths,
+    pattern = map(static_paths),
+    format = "file"
+  ),
 
   # fuse targets depend on blog configuration files
   # copy targets don't need dependencies
@@ -41,5 +65,9 @@ list(
     ),
     pattern = map(post_files)
   ),
-  tar_target(static_copy, blog$copy_static(static_files), pattern = map(static_files))
+  tar_target(
+    name = static_copy,
+    command = blog$copy_static(static_files),
+    pattern = map(static_files)
+  )
 )
