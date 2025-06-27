4df5610d80c3124264c5aaae68d74bcd7fa2cdda
Danielle Navarro <djnavarro@protonmail.com>
2025-06-28 00:54:17 +1000

    adds a few classes

--- posts/2025-06-27_quarto-syntax-from-r/class.R
+++ posts/2025-06-27_quarto-syntax-from-r/class.R
@@ -1,4 +1,6 @@
 
+# tabs and sections -------------------------------------------
+
 quarto_section <- function(.title, .level) {
   structure(
     rlang::list2(
@@ -10,11 +12,11 @@ quarto_section <- function(.title, .level) {
 }
 
 quarto_tabset <- function(..., .level = 3L) {
-  tabs <- rlang::list2(...)
+  .content <- rlang::list2(...)
   structure(
     rlang::list2(
-      content = tabs,
-      title = names(tabs),
+      content = .content,
+      title = names(.content),
       level = .level
     ),
     class = "quarto_tabset"
@@ -33,12 +35,57 @@ quarto_tabsec <- function(.content, .title, .level = 3L) {
   )
 }
 
-quarto_collection <- function(...) {
+# groups of output -------------------------------------------
+
+quarto_output <- function(...) {
   structure(
     rlang::list2(
-      collection = rlang::list2(...)
+      content = rlang::list2(...)
     ),
-    class = "quarto_collection"
+    class = "quarto_output"
   )
 }
 
+quarto_markdown <- function(..., .sep = "") {
+  structure(
+    rlang::list2(
+      content = rlang::list2(...),
+      sep = .sep,
+    ),
+    class = "quarto_markdown"
+  )
+}
+
+quarto_paragraph <- function(..., .sep = "") {
+  structure(
+    rlang::list2(
+      content = rlang::list2(...),
+      sep = .sep,
+    ),
+    class = "quarto_paragraph"
+  )
+}
+
+# divs and spans -------------------------------------------
+
+# TODO: should this be a .content or dots?
+quarto_div <- function(..., .class, .sep = "") {
+  structure(
+    rlang::list2(
+      content = rlang::list2(...),
+      class = .class,
+      sep = .sep,
+    ),
+    class = "quarto_div"
+  )
+}
+
+quarto_span <- function(.content, .class = NULL) {
+  structure(
+    rlang::list2(
+      content = .content,
+      class = .class
+    ),
+    class = "quarto_span"
+  )
+}
--- posts/2025-06-27_quarto-syntax-from-r/coerce.R
+++
@@ -1,7 +0,0 @@
-as_quarto_tabset <- function(x, .level = 3L) {
-  do.call(quarto_tabset, x)
-}
-
-as_quarto_section <- function(x, .level = 3L) {
-  quarto_section(as.character(x), .level)
-}
